5034a4ea59922face8bde0f134e0adf66a6fc122
oia2 <135359658+oia2@users.noreply.github.com>
2024-02-04 00:04:11 +0700

    Add files via upload

---
+++ Cuda/CudaRuntime1/kernel.cu
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cuda_runtime.h>
+#include <chrono>
+
+__global__ void computeChannels(const uchar* img, uchar* blueChannel, uchar* yellowChannel, int rows, int cols) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < cols && y < rows) {
+        int offset = y * cols + x;
+
+        uchar redv = img[offset * 3 + 2];
+        uchar greenv = img[offset * 3 + 1];
+        uchar bluev = img[offset * 3];
+
+        // Вычисление модифицированного синего канала
+        uchar Bv = bluev - (greenv + bluev) / 2;
+
+        // Вычисление модифицированного желтого канала
+        uchar Yv = redv + greenv - 2 * (abs(redv - greenv) + bluev);
+
+        // Запись результатов в соответствующие Mat-объекты
+        blueChannel[offset] = Bv;
+        yellowChannel[offset] = Yv;
+    }
+}
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cv::Mat image = cv::imread("../src/giena_1024x768.jpg");
+
+        if (image.empty()) {
+            std::cout << "Failed to upload image" << std::endl;
+            return -1;
+        }
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        // Создание двух пустых Mat-объектов для модифицированных синего и желтого каналов
+        cv::Mat modifiedBlueChannel = cv::Mat::zeros(image.size(), CV_8U);
+        cv::Mat modifiedYellowChannel = cv::Mat::zeros(image.size(), CV_8U);
+
+
+        uchar* d_img;
+        cudaMalloc((void**)&d_img, image.rows * image.cols * 3);
+        cudaMemcpy(d_img, image.data, image.rows * image.cols * 3, cudaMemcpyHostToDevice);
+
+        uchar* d_blueChannel, * d_yellowChannel;
+        cudaMalloc((void**)&d_blueChannel, image.rows * image.cols);
+        cudaMalloc((void**)&d_yellowChannel, image.rows * image.cols);
+
+        const dim3 block(32, 32);
+        const dim3 grid((image.cols + block.x - 1) / block.x, (image.rows + block.y - 1) / block.y);
+
+
+        // Запуск ядра на устройстве
+        computeChannels << <grid, block >> > (d_img, d_blueChannel, d_yellowChannel, image.rows, image.cols);
+        cudaDeviceSynchronize();
+
+        // Копирование результатов обратно
+        cudaMemcpy(modifiedBlueChannel.data, d_blueChannel, image.rows * image.cols, cudaMemcpyDeviceToHost);
+        cudaMemcpy(modifiedYellowChannel.data, d_yellowChannel, image.rows * image.cols, cudaMemcpyDeviceToHost);
+        // Замер времени заканчивается
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << i << ". Time " << duration.count() << " ms" << std::endl;
+        // Освобождение выделенной памяти на устройстве
+        cudaFree(d_img);
+        cudaFree(d_blueChannel);
+        cudaFree(d_yellowChannel);
+
+        // Сохранение изображений в файлы
+        cv::imwrite("../modified_blue_channel.jpg", modifiedBlueChannel);
+        cv::imwrite("../modified_yellow_channel.jpg", modifiedYellowChannel);
+    }
+    return 0;
+}
---
+++ Cuda/CudaRuntime1/task2.cu
@@ -0,0 +1,107 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <chrono>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCK_SIZE 32 
+
+__global__ void matrixMult(const __int64* Am, const __int64* Bm, __int64* result, int size) {
+	int bx = blockIdx.x;
+	int by = blockIdx.y;
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+
+	int ia = size * (gridDim.y * by + ty);
+	int ib = gridDim.x * bx + tx;
+	int ic = ia + ib;
+
+	__int64 sum = 0;
+
+	for (int k = 0; k < size; k++) {
+		sum += Am[ia + k] * Bm[k * size + ib];
+	}
+	result[ic] = sum;
+}
+
+
+void compareMatrix(const __int64* f, const __int64* s, int size) {
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j) {
+			if (f[i * size + j] != s[i * size + j]) {
+				printf("Matrixes not equal!\n");
+				return;
+			}
+		}
+	}
+	printf("Matrices are equal!\n");
+}
+
+int main()
+{
+	int size = 1024;
+
+	size_t byte_size = size * size * sizeof(__int64);
+	__int64* Am = (__int64*)malloc(byte_size);
+	__int64* Bm = (__int64*)malloc(byte_size);
+	__int64* GPU_C = (__int64*)malloc(byte_size);
+	__int64* CPU_C = (__int64*)malloc(byte_size);
+
+	for (int i = 0; i < size * size; ++i) {
+		Am[i] = rand() % 10;
+		Bm[i] = rand() % 10;
+		CPU_C[i] = 0;
+	}
+
+	printf("Scalar: \n");
+	auto start = std::chrono::system_clock::now();
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j) {
+			for (int k = 0; k < size; ++k) {
+				CPU_C[i * size + j] += Am[i * size + k] * Bm[k * size + j];
+			}
+		}
+	}
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> diff = end - start;
+
+	printf("Time: %f seconds\n", diff);
+
+	printf("GPU: \n");
+
+	__int64* d_A = NULL;
+	cudaMalloc((void**)&d_A, byte_size);
+	cudaMemcpy(d_A, Am, byte_size, cudaMemcpyHostToDevice);
+
+	__int64* d_B = NULL;
+	cudaMalloc((void**)&d_B, byte_size);
+	cudaMemcpy(d_B, Bm, byte_size, cudaMemcpyHostToDevice);
+
+	__int64* d_C = NULL;
+	cudaMalloc((void**)&d_C, byte_size);
+
+	start = std::chrono::system_clock::now();
+
+	const dim3 block(32, 32);
+	const dim3 grid((size) / block.x, (size) / block.y);
+	matrixMult << < grid, block >> > (d_A, d_B, d_C, size);
+
+	cudaDeviceSynchronize();
+
+	end = std::chrono::system_clock::now();
+	diff = end - start;
+	cudaMemcpy(GPU_C, d_C, byte_size, cudaMemcpyDeviceToHost);
+
+
+	printf("Time: %f seconds\n", diff);
+	compareMatrix(GPU_C, CPU_C, size);
+
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+	free(Am);
+	free(Bm);
+	free(GPU_C);
+	free(CPU_C);
+	return 0;
+}
